75d17a549e5501107aad67992e502ba971de6183
Jim Bankoski <jimbankoski@google.com>
2015-01-16 15:14:12 -0800

    reformat to be more google stylish
    Change-Id: I1e464e041380888c829662266db834a05c4caa84

--- rtp.h
+++ rtp.h
@@ -16,56 +16,54 @@ typedef enum {
   VPX_VP9
 } CODEC;
 
-enum
-{
-    DATAPACKET = 0,
-    XORPACKET = 1,
+enum {
+  DATAPACKET = 0,
+  XORPACKET = 1,
 };
-enum
-{
-    NORMAL = 0,
-    KEY = 1,
-    GOLD = 2,
-    ALTREF = 3
+enum {
+  NORMAL = 0,
+  KEY = 1,
+  GOLD = 2,
+  ALTREF = 3
 };
-enum
-{
-    LOG_PACKET = 1,
-    SKIP   = 2,
-    REBUILD = 4,
-    DISCARD = 8,
-    FRAME   = 16,
-    ERRORS = 32,
+enum {
+  LOG_PACKET = 1,
+  SKIP = 2,
+  REBUILD = 4,
+  DISCARD = 8,
+  FRAME = 16,
+  ERRORS = 32,
 };
 
+//#define LOG_MASK  ( ERRORS |REBUILD|DISCARD|LOG_PACKET|SKIP|FRAME)
 
-#define LOG_MASK  ( ERRORS |REBUILD|DISCARD|LOG_PACKET|SKIP|FRAME) // ( ERRORS|LOG_PACKET|FRAME|SKIP|REBUILD|DISCARD ) //
-#define R2(X) ( (((X)&0xff)<<8) | ((X)>>8) )
-#define R4(X) ( ((X&0xff)<< 24) | ((X&0xff00) << 8) |((X&0xff0000) >> 8) | (X>>24) )
-typedef struct
-{
-    int version : 2;
-    int pad : 1;
-    int extension : 1;
-    int csrccount : 4;
-    int marker : 1;
-    int payloadtype : 7;
-    unsigned short seq;
-    unsigned int timestamp;
+#define LOG_MASK  (ERRORS)
+#define R2(X) ( (((X) & 0xff) << 8) | ((X) >> 8) )
+#define R4(X) ( ((X & 0xff) << 24) | ((X & 0xff00) << 8) | \
+                ((X & 0xff0000) >> 8) | (X >> 24) )
+typedef struct {
+  int version :2;
+  int pad :1;
+  int extension :1;
+  int csrccount :4;
+  int marker :1;
+  int payloadtype :7;
+  unsigned short seq;
+  unsigned int timestamp;
 
-    unsigned int ssrc ;
-    unsigned int csrc ;  // repeated up to 15 times
+  unsigned int ssrc;
+  unsigned int csrc;  // repeated up to 15 times
 
-    unsigned int type : 1;
-    unsigned int redundant_count : 3;
-    unsigned int new_frame: 1;
-    unsigned int end_frame: 1;
-    unsigned int frame_type: 2;
+  unsigned int type :1;
+  unsigned int redundant_count :3;
+  unsigned int new_frame :1;
+  unsigned int end_frame :1;
+  unsigned int frame_type :2;
 
-    unsigned char data[PACKET_SIZE];
+  unsigned char data[PACKET_SIZE];
 
-    // this value doesn't actually get written or read
-    unsigned int size;
+  // this value doesn't actually get written or read
+  unsigned int size;
 
 } PACKET;
 
